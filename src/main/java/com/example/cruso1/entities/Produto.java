f6cd36e81b887ed4fff29dab2ba7b7f180d47b0c
Fabiofcn <fabiofcn@gmail.com>
2020-02-24 15:50:54 -0300

    ProjetoCrudteste

--- src/main/java/com/example/cruso1/entities/Produto.java
+++ src/main/java/com/example/cruso1/entities/Produto.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_produto")
 public class Produto implements Serializable {
-
+ //fabio
 	private static final long serialVersionUID = 1L;
 	
 	@Id
